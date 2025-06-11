fde1686d8e6b8d992ffa7d69eca286a9141c7297
Stephane Monier <smonier@jahia.com>
2025-06-11 11:40:47 +0200

    updated readme

--- src/javascript/ImportContentFromJson/ImportContent.component.jsx
+++ src/javascript/ImportContentFromJson/ImportContent.component.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import Papa from 'papaparse';
+import Papa from 'papaparse/papaparse.min.js';
 import {useLazyQuery, useMutation} from '@apollo/client';
 import {
     GetContentTypeQuery,
