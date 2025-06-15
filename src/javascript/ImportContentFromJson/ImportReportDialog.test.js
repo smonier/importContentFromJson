33c06fee3a03c00d50cda1d0c8d9ebbf7f9b8028
Stephane Monier <smonier@Jahia.com>
2025-06-15 18:40:14 +0200

    feat: show node path in import report

---
+++ src/javascript/ImportContentFromJson/ImportReportDialog.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import ImportReportDialog from './ImportReportDialog.jsx';
+import en from '../../main/resources/javascript/locales/en.json';
+
+describe('ImportReportDialog', () => {
+    const t = key => {
+        const stripped = key.replace('label.', '');
+        return en.label[stripped] || key;
+    };
+
+    test('renders node path column', () => {
+        const report = {
+            images: [{name: 'img.png', status: 'created', node: '/content/imgNode'}]
+        };
+        const html = ReactDOMServer.renderToStaticMarkup(
+            <ImportReportDialog open={true} onClose={() => {}} report={report} t={t}/>
+        );
+
+        expect(html).toContain(en.label.nodePath);
+        expect(html).toContain('/content/imgNode');
+    });
+});
